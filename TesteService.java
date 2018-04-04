7958cc3b33dd76c41cbf61e281686c7455844967
edicleidetlima <38087652+edicleidetlima@users.noreply.github.com>
2018-04-04 20:29:16 -0300

    Incluindo teste service

---
+++ TesteService.java
@@ -0,0 +1,16 @@
+package br.com.geladaonline.services;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/olaMundo")
+public class TesteService {
+	
+	@GET
+	@Produces("text/plain")
+	public String disOla(){
+		return "Olá Mundo Rest!";
+	}
+
+}
